14184fdba3dfe023b7f83846a492e664bedf6244
clee088 <42843045+clee088@users.noreply.github.com>
2019-01-08 09:11:41 -0500

    Updated from Brython Server: 1/8/2019 9:11:40 AM

--- python_finance_program.py
+++ python_finance_program.py
@@ -3,44 +3,86 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+from bs4 import BeautifulSoup
+import requests
+from math import floor
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
 #============================PFP_COMMANDS============================
 def help():
     print('''
-Press t for a summary of a ticker symbol
-Press y for a graph of the current yield Curve
-Press h for a list of commands
-Press q to quit
+Press 't' for a summary of a ticker symbol
+Press 'l' for a list of popular stocks
+Press 'y' for a graph of the current yield curve
+Press 'f' for US Treasury Data
+Press 'h' for a list of commands
+Press 'q' to quit
 ''')
+#-------------------------------------------------------------------
 def quit():
     check = input('Are you sure you want to quit? y/n ')
-    if check == 'y':
-        print('Quitting...')
-    elif check == 'n':
-        pfp()
-    else:
-        print('Did not understand. Please try again')
-        quit()
+    for r in check:
+        if check == 'y':
+            print('Quitting...')
+        elif check == 'n':
+            pfp()
+        else:
+            print('Did not understand. Please try again')
+            quit()
 #=======================================================================
 def yield_curve():
-    onemonth = 2.37
+    onemonth = 2.42
     twomonth = 2.42
-    threemonth = 2.42
-    sixmonth = 2.58
-    one = 2.71
-    two = 2.80
-    three = 2.81
-    five = 2.79
-    seven = 2.84
-    ten = 2.91
-    twenty = 3.05
-    thirty = 3.16
+    threemonth = 2.45
+    sixmonth = 2.54
+    one = 2.58
+    two = 2.53
+    three = 2.51
+    five = 2.53
+    seven = 2.60
+    ten = 2.70
+    twenty = 2.86
+    thirty = 2.99
     plt.plot([2, 3, 5, 7, 10, 20, 30], [two, three, five, seven, ten, twenty, thirty])
     plt.plot([2, 3, 5, 7, 10, 20, 30], [two, three, five, seven, ten, twenty, thirty], 'ro')
-    plt.axis([0, 32, 2.6, 3.25])
+    plt.axis([0, 32, floor(three), 3.25])
     plt.ylabel("U.S. Treasury Bond's Yields to Maturity")
     plt.xlabel("U.S. Treasury Bonds' Times to Maturity (years)")
     plt.show()
+#=======================================================================
+def fred():
+    data = requests.get('https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield')
 
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    d = soup.find(class_='t-chart')
+
+    #print(d)
+    print(d.prettify())
+#=======================================================================
+def los():
+    print('''
+List of Popular Stocks:
+AAPL - Apple
+GOOGL - Google
+TSLA - Tesla
+SBUX - Starbucks
+AMZN - Amazon
+DIS - The Walt Disney Company
+T - AT&T
+VZ - Verizon
+TMUS - T-Mobile
+NFLX - Netflix
+MSFT - Microsoft
+    ''')
+#=====================================================
 def summary():
     current = dt.datetime.now()
 
@@ -48,15 +90,51 @@ def summary():
     ticker = ticker.upper()
 
     style.use = 'ggplot'
-    start = dt.datetime(current.year, current.month - 1, current.day)
-    end = dt.datetime(current.year, current.month, current.day)
-
-    df = web.DataReader(ticker, 'yahoo', start, end)
+    if current.month == 1:
+        start = dt.datetime(current.year - 1, current.month, current.day)
+        end = dt.datetime(current.year, current.month, current.day)
+    else:
+        start = dt.datetime(current.year, current.month - 6, current.day)
+        end = dt.datetime(current.year, current.month, current.day)
+    t = web.DataReader(ticker, 'yahoo', start, end)
     print('')
     print('Getting data for ticker symbol "' + ticker + '"...')
     print(ticker)
-    print(df)
+    print(t)
+    graph = input("Press 'g' to graph / Press 'c' to compare with another stock / Press any key to continue")
+    for i in graph:
+        if i == 'g':
+            t['Adj Close'].plot(label = str(ticker), figsize=(16, 8), title = str(ticker))
+            plt.legend();
+            plt.show()
+        elif i == 'c':
+            current = dt.datetime.now()
 
+            cticker = input('Ticker symbol:  ')
+            cticker = cticker.upper()
+
+            style.use = 'ggplot'
+            if current.month == 1:
+                start = dt.datetime(current.year - 1, current.month, current.day)
+                end = dt.datetime(current.year, current.month, current.day)
+            else:
+                start = dt.datetime(current.year, current.month - 6, current.day)
+                end = dt.datetime(current.year, current.month, current.day)
+            compare = web.DataReader(cticker, 'yahoo', start, end)
+
+            t['Adj Close'].plot(label = str(ticker), figsize=(16, 8), title = str(ticker + " / " + cticker))
+            compare['Adj Close'].plot(label = str(cticker))
+            plt.legend();
+            plt.show()
+
+#=====================================================
+def debug():
+    current = dt.datetime.now()
+
+    currentdate = "{}/{}/{}".format(current.year, current.month, current.day)
+
+    print(currentdate)
+#=========================PFP==========================
 def pfp():
     run = input('''Enter command: ''')
     for i in run:
@@ -64,14 +142,22 @@ def pfp():
             summary()
             print('')
             pfp()
+        elif i == 'l':
+            los()
+            pfp()
+        elif i == 'f':
+            fred()
+            pfp()
         elif i == 'y':
             yield_curve()
             pfp()
         elif i == 'q':
             quit()
         elif i == 'd':
-            print('Debugging')
-        elif i == 'h':
+            print('Debugging...')
+            debug()
+            pfp()
+        elif i == 'h'or i == 'help':
             help()
             pfp()
         else:
